091a6a429ae803fa30d2b026101731f4f6570c8f
sistemanixa <nixa@sistemanixa.com>
2026-07-04 13:36:58 -0300

    Update cache version from v6.5 to v6.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'convivio-v6.5';
+const CACHE = 'convivio-v6.6';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
